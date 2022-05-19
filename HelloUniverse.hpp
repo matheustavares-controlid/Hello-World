0f5281fdfc1ee2ffb5fe66ff39f932da3df79d65
matheustavares-controlid <matheus.andrade@controlid.com.br>
2022-05-19 15:07:55 -0300

    Random branch

---
+++ HelloUniverse.hpp
@@ -0,0 +1,6 @@
+#ifndef HELLOUNIVERSE_HPP
+#define HELLOUNIVERSE_HPP
+
+void HelloUniverse();
+
+#endif /* HELLOUNIVERSE_HPP */
--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <HelloUniverse.hpp>
 
 int main() {
 
     std::cout << "Hello, World!" << std::endl;
-
+    HelloUniverse();
+    
     return 0;
 }
